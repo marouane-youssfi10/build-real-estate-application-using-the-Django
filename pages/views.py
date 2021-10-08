ae010892c703c599ca0f6cd99dfd34315ad6c44d
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-10-08 18:55:59 +0100

    create templates with two pages html index and about

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>hello world</h1>')
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')

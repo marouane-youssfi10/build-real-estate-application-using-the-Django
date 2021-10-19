aaf75237459007829203eb172a405753ac88137c
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-10-19 13:55:43 +0100

    update listings and realtors admin

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Listing
 # Register your models here.
 
-admin.site.register(Listing)
+class ListingsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+
+admin.site.register(Listing, ListingsAdmin)

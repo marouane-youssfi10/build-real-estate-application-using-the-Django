a2b866137397a2a723f10e8761222126b2827c50
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-10-19 21:59:04 +0100

    update listing.html for app listings

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, Paginator
+
 # Create your views here.
 
 def index(request):
@@ -16,7 +17,12 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')

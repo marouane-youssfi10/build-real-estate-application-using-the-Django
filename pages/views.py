74909b01c9ab742736e322f3ad2bdd721f1a8b2e
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-10-20 12:23:50 +0100

    getting price_choices and state_choices and bedrooms_choices from database to putting in index.html

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
 # Create your views here.
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
     }
     return render(request, 'pages/index.html', context)
 

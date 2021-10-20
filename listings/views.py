9c189edcef96c280ddac5414f36a9e3d269524c7
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-10-20 13:45:01 +0100

    updating index.html page

--- listings/views.py
+++ listings/views.py
@@ -63,6 +63,7 @@ def search(request):
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
-        'listings': queryset_list
+        'listings': queryset_list,
+        'values': request.GET
     }
     return render(request, 'listings/search.html', context)

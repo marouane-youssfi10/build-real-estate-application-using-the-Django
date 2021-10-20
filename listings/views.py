8c71e276379da0e67615a0e6824ed03bc0ec1cdb
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-10-20 12:35:26 +0100

    updating search.html page

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 
 # Create your views here.
 
@@ -25,4 +26,10 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+    return render(request, 'listings/search.html', context)

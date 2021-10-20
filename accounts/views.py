ac4b53e0fc9eba8151213f9c92d68437641a47a5
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-10-20 23:02:51 +0100

    making register.html dynamic

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,45 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check of password match
+        if password == password2:
+            # check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                print('ccc = ', messages.error(request, 'That username is taken'))
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    # looks good
+                    user = User.objects.create_user(username=username, password=password, email=email,
+                                                    first_name=first_name, last_name=last_name)
+                    # Login after register
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in')
+                    # return redirect('index')
+                    user.save()
+                    messages.success(request, 'You are now registered and cal log in')
+                    return redirect('login')
+
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 

1f2e6aa7230050742b4814a960e335b3541a38d9
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-10-21 15:44:38 +0100

    customize inquiry check for avoiding sending multiple messages

--- contacts/views.py
+++ contacts/views.py
@@ -15,6 +15,14 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        # Check if user has made inquiry already
+        if request.user.is_authenticated:
+            user_id= request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                message.error(request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/' + listing_id)
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message,
                           user_id=user_id)
 
